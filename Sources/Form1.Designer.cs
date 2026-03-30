aeb3df1da5588dcda38f0c93d0b893e0f9393f78
4efirch1k <nullpaperoska@gmail.com>
2026-03-30 19:09:14 +0300

    Добавлен исходный файл свойства и значения параметров элементов Формы

---
+++ Sources/Form1.Designer.cs
@@ -0,0 +1,220 @@
+﻿using System.Windows.Forms;
+
+namespace Practicheskaya
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.AddButton = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.upadateDataButton = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.updateDataBaseList = new System.Windows.Forms.Button();
+            this.exportDB = new System.Windows.Forms.Button();
+            this.deleteFromDB = new System.Windows.Forms.Button();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.tabPage2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(12, 12);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(776, 426);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.AddButton);
+            this.tabPage1.Controls.Add(this.comboBox1);
+            this.tabPage1.Controls.Add(this.upadateDataButton);
+            this.tabPage1.Controls.Add(this.progressBar1);
+            this.tabPage1.Controls.Add(this.dataGridView1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(768, 400);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Рынок";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // AddButton
+            // 
+            this.AddButton.Location = new System.Drawing.Point(590, 260);
+            this.AddButton.Name = "AddButton";
+            this.AddButton.Size = new System.Drawing.Size(120, 40);
+            this.AddButton.TabIndex = 6;
+            this.AddButton.Text = "Добавить";
+            this.AddButton.UseVisualStyleBackColor = true;
+            this.AddButton.Click += new System.EventHandler(this.AddButton_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Валюта [USD]",
+            "Валюта [EUR]",
+            "Валюта [RUB]",
+            "Валюта [KZT]"});
+            this.comboBox1.SelectedIndex = 0;
+            this.comboBox1.Location = new System.Drawing.Point(590, 180);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 5;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // upadateDataButton
+            // 
+            this.upadateDataButton.Location = new System.Drawing.Point(590, 70);
+            this.upadateDataButton.Name = "upadateDataButton";
+            this.upadateDataButton.Size = new System.Drawing.Size(120, 40);
+            this.upadateDataButton.TabIndex = 2;
+            this.upadateDataButton.Text = "Обновить";
+            this.upadateDataButton.UseVisualStyleBackColor = true;
+            this.upadateDataButton.Click += new System.EventHandler(this.upadateDataButton_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(212, 171);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(100, 23);
+            this.progressBar1.TabIndex = 1;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(520, 400);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.updateDataBaseList);
+            this.tabPage2.Controls.Add(this.exportDB);
+            this.tabPage2.Controls.Add(this.deleteFromDB);
+            this.tabPage2.Controls.Add(this.dataGridView2);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(768, 400);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Избранное";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // updateDataBaseList
+            // 
+            this.updateDataBaseList.Location = new System.Drawing.Point(590, 130);
+            this.updateDataBaseList.Name = "updateDataBaseList";
+            this.updateDataBaseList.Size = new System.Drawing.Size(120, 44);
+            this.updateDataBaseList.TabIndex = 4;
+            this.updateDataBaseList.Text = "Обновить";
+            this.updateDataBaseList.UseVisualStyleBackColor = true;
+            this.updateDataBaseList.Click += new System.EventHandler(this.updateDataBaseList_Click);
+            // 
+            // exportDB
+            // 
+            this.exportDB.Location = new System.Drawing.Point(590, 180);
+            this.exportDB.Name = "exportDB";
+            this.exportDB.Size = new System.Drawing.Size(120, 40);
+            this.exportDB.TabIndex = 3;
+            this.exportDB.Text = "Экспорт";
+            this.exportDB.UseVisualStyleBackColor = true;
+            this.exportDB.Click += new System.EventHandler(this.buttonExportJson_Click);
+            // 
+            // deleteFromDB
+            // 
+            this.deleteFromDB.Location = new System.Drawing.Point(590, 226);
+            this.deleteFromDB.Name = "deleteFromDB";
+            this.deleteFromDB.Size = new System.Drawing.Size(120, 40);
+            this.deleteFromDB.TabIndex = 2;
+            this.deleteFromDB.Text = "Удалить";
+            this.deleteFromDB.UseVisualStyleBackColor = true;
+            this.deleteFromDB.Click += new System.EventHandler(this.deleteFromDB_Click);
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.AllowUserToAddRows = false;
+            this.dataGridView2.AllowUserToDeleteRows = false;
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.ReadOnly = true;
+            this.dataGridView2.Size = new System.Drawing.Size(520, 400);
+            this.dataGridView2.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.tabPage2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Button upadateDataButton;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.Button deleteFromDB;
+        private System.Windows.Forms.Button exportDB;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private Button AddButton;
+        private Button updateDataBaseList;
+    }
+}
+
